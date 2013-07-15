12e3648d77e7b79962fedfaf5e90e056e08ad539
mevanoff@google.com <mevanoff@google.com@0838571c-615c-c453-dd1f-8e7db030a593>
2013-07-15 18:29:52 +0000

    Shortened the script using a for-loop

--- BuildTheBaseGlobe/vector/TigerLineRoads/5_AddTigerResourcesToProject.sh
+++ BuildTheBaseGlobe/vector/TigerLineRoads/5_AddTigerResourcesToProject.sh
@@ -1,49 +1,11 @@
-geaddtovectorproject -o Projects/Vector --template ALABAMA.khdsp Resources/Vector/Roads/TigerLine2009/ALABAMA
-geaddtovectorproject -o Projects/Vector --template ALASKA.khdsp Resources/Vector/Roads/TigerLine2009/ALASKA
-geaddtovectorproject -o Projects/Vector --template ARIZONA.khdsp Resources/Vector/Roads/TigerLine2009/ARIZONA
-geaddtovectorproject -o Projects/Vector --template ARKANSAS.khdsp Resources/Vector/Roads/TigerLine2009/ARKANSAS
-geaddtovectorproject -o Projects/Vector --template CALIFORNIA.khdsp Resources/Vector/Roads/TigerLine2009/CALIFORNIA
-geaddtovectorproject -o Projects/Vector --template COLORADO.khdsp Resources/Vector/Roads/TigerLine2009/COLORADO
-geaddtovectorproject -o Projects/Vector --template CONNECTICUT.khdsp Resources/Vector/Roads/TigerLine2009/CONNECTICUT
-geaddtovectorproject -o Projects/Vector --template DELAWARE.khdsp Resources/Vector/Roads/TigerLine2009/DELAWARE
-geaddtovectorproject -o Projects/Vector --template DISTRICT_OF_COLUMBIA.khdsp Resources/Vector/Roads/TigerLine2009/DISTRICT_OF_COLUMBIA
-geaddtovectorproject -o Projects/Vector --template FLORIDA.khdsp Resources/Vector/Roads/TigerLine2009/FLORIDA
-geaddtovectorproject -o Projects/Vector --template GEORGIA.khdsp Resources/Vector/Roads/TigerLine2009/GEORGIA
-geaddtovectorproject -o Projects/Vector --template HAWAII.khdsp Resources/Vector/Roads/TigerLine2009/HAWAII
-geaddtovectorproject -o Projects/Vector --template IDAHO.khdsp Resources/Vector/Roads/TigerLine2009/IDAHO
-geaddtovectorproject -o Projects/Vector --template ILLINOIS.khdsp Resources/Vector/Roads/TigerLine2009/ILLINOIS
-geaddtovectorproject -o Projects/Vector --template INDIANA.khdsp Resources/Vector/Roads/TigerLine2009/INDIANA
-geaddtovectorproject -o Projects/Vector --template IOWA.khdsp Resources/Vector/Roads/TigerLine2009/IOWA
-geaddtovectorproject -o Projects/Vector --template KANSAS.khdsp Resources/Vector/Roads/TigerLine2009/KANSAS
-geaddtovectorproject -o Projects/Vector --template KENTUCKY.khdsp Resources/Vector/Roads/TigerLine2009/KENTUCKY
-geaddtovectorproject -o Projects/Vector --template LOUISIANA.khdsp Resources/Vector/Roads/TigerLine2009/LOUISIANA
-geaddtovectorproject -o Projects/Vector --template MAINE.khdsp Resources/Vector/Roads/TigerLine2009/MAINE
-geaddtovectorproject -o Projects/Vector --template MARYLAND.khdsp Resources/Vector/Roads/TigerLine2009/MARYLAND
-geaddtovectorproject -o Projects/Vector --template MASSACHUSETTS.khdsp Resources/Vector/Roads/TigerLine2009/MASSACHUSETTS
-geaddtovectorproject -o Projects/Vector --template MICHIGAN.khdsp Resources/Vector/Roads/TigerLine2009/MICHIGAN
-geaddtovectorproject -o Projects/Vector --template MINNESOTA.khdsp Resources/Vector/Roads/TigerLine2009/MINNESOTA
-geaddtovectorproject -o Projects/Vector --template MISSISSIPPI.khdsp Resources/Vector/Roads/TigerLine2009/MISSISSIPPI
-geaddtovectorproject -o Projects/Vector --template MISSOURI.khdsp Resources/Vector/Roads/TigerLine2009/MISSOURI
-geaddtovectorproject -o Projects/Vector --template MONTANA.khdsp Resources/Vector/Roads/TigerLine2009/MONTANA
-geaddtovectorproject -o Projects/Vector --template NEBRASKA.khdsp Resources/Vector/Roads/TigerLine2009/NEBRASKA
-geaddtovectorproject -o Projects/Vector --template NEVADA.khdsp Resources/Vector/Roads/TigerLine2009/NEVADA
-geaddtovectorproject -o Projects/Vector --template NEW_HAMPSHIRE.khdsp Resources/Vector/Roads/TigerLine2009/NEW_HAMPSHIRE
-geaddtovectorproject -o Projects/Vector --template NEW_JERSEY.khdsp Resources/Vector/Roads/TigerLine2009/NEW_JERSEY
-geaddtovectorproject -o Projects/Vector --template NEW_MEXICO.khdsp Resources/Vector/Roads/TigerLine2009/NEW_MEXICO
-geaddtovectorproject -o Projects/Vector --template NEW_YORK.khdsp Resources/Vector/Roads/TigerLine2009/NEW_YORK
-geaddtovectorproject -o Projects/Vector --template NORTH_CAROLINA.khdsp Resources/Vector/Roads/TigerLine2009/NORTH_CAROLINA
-geaddtovectorproject -o Projects/Vector --template NORTH_DAKOTA.khdsp Resources/Vector/Roads/TigerLine2009/NORTH_DAKOTA
-geaddtovectorproject -o Projects/Vector --template OHIO.khdsp Resources/Vector/Roads/TigerLine2009/OHIO
-geaddtovectorproject -o Projects/Vector --template OKLAHOMA.khdsp Resources/Vector/Roads/TigerLine2009/OKLAHOMA
-geaddtovectorproject -o Projects/Vector --template OREGON.khdsp Resources/Vector/Roads/TigerLine2009/OREGON
-geaddtovectorproject -o Projects/Vector --template PENNSYLVANIA.khdsp Resources/Vector/Roads/TigerLine2009/PENNSYLVANIA
-geaddtovectorproject -o Projects/Vector --template RHODE_ISLAND.khdsp Resources/Vector/Roads/TigerLine2009/RHODE_ISLAND
-geaddtovectorproject -o Projects/Vector --template SOUTH_CAROLINA.khdsp Resources/Vector/Roads/TigerLine2009/SOUTH_CAROLINA
-geaddtovectorproject -o Projects/Vector --template SOUTH_DAKOTA.khdsp Resources/Vector/Roads/TigerLine2009/SOUTH_DAKOTA
-geaddtovectorproject -o Projects/Vector --template TENNESSEE.khdsp Resources/Vector/Roads/TigerLine2009/TENNESSEE
-geaddtovectorproject -o Projects/Vector --template TEXAS.khdsp Resources/Vector/Roads/TigerLine2009/TEXAS
-geaddtovectorproject -o Projects/Vector --template UTAH.khdsp Resources/Vector/Roads/TigerLine2009/UTAH
-geaddtovectorproject -o Projects/Vector --template VERMONT.khdsp Resources/Vector/Roads/TigerLine2009/VERMONT
-geaddtovectorproject -o Projects/Vector --template VIRGINIA.khdsp Resources/Vector/Roads/TigerLine2009/VIRGINIA
-geaddtovectorproject -o Projects/Vector --template WASHINGTON.khdsp Resources/Vector/Roads/TigerLine2009/WASHINGTON
-geaddtovectorproject -o Projects/Vector --template WEST_VIRGINIA.khdsp Resources/Vector/Roads/TigerLine2009/WEST_VIRGINIA
+for USState in ALABAMA ALASKA ARIZONA ARKANSAS CALIFORNIA COLORADO CONNECTICUT DELAWARE DISTRICT_OF_COLUMBIA FLORIDA GEORGIA HAWAII IDAHO ILLINOIS INDIANA IOWA KANSAS KENTUCKY LOUISIANA MAINE MARYLAND MASSACHUSETTS MICHIGAN MINNESOTA MISSISSIPPI MISSOURI MONTANA NEBRASKA NEVADA NEW_HAMPSHIRE NEW_JERSEY NEW_MEXICO NEW_YORK NORTH_CAROLINA NORTH_DAKOTA OHIO OKLAHOMA OREGON PENNSYLVANIA RHODE_ISLAND SOUTH_CAROLINA SOUTH_DAKOTA TENNESSEE TEXAS UTAH VERMONT VIRGINIA WASHINGTON WEST_VIRGINIA 
+do
+
+      geaddtovectorproject -o Projects/Vector --template $USState.khdsp Resources/Vector/Roads/TigerLine2009/$USState
+
+done
+
+
+
+
+
