b468e7700e41bc749e724261757a6ae4a09158f5
mevanoff@google.com <mevanoff@google.com@0838571c-615c-c453-dd1f-8e7db030a593>
2013-07-15 17:21:03 +0000

    Initial Import

---
+++ trunk/BuildTheBaseGlobe/vector/OpenStreetmap/FetchOSMData.sh
@@ -0,0 +1,232 @@
+# Fetch all of the OpenStreetmap non-USA roads data from Cloudmade.com. USA data will be fetched from US Census.
+#
+# If you want USA data from OSM, just do a wget -r http://downloads.cloudmade.com/north_america/united_states
+
+wget -nH -x http://downloads.cloudmade.com/africa/sudan/sudan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/botswana/botswana.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/burundi/burundi.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/sierra_leone/sierra_leone.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/kenya/kenya.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/equatorial_guinea/equatorial_guinea.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/namibia/namibia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/ghana/ghana.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/seychelles/seychelles.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/morocco/morocco.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/cameroon/cameroon.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/juan_de_nova_island/juan_de_nova_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/glorioso_islands/glorioso_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/lesotho/lesotho.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/benin/benin.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/chad/chad.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/togo/togo.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/egypt/egypt.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/somalia/somalia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/sao_tome_and_principe/sao_tome_and_principe.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/guinea/guinea.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/comoros/comoros.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/cape_verde/cape_verde.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/ivory_coast/ivory_coast.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/uganda/uganda.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/zambia/zambia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/algeria/algeria.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/mali/mali.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/western_sahara/western_sahara.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/malawi/malawi.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/reunion/reunion.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/eritrea/eritrea.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/mauritius/mauritius.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/madagascar/madagascar.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/angola/angola.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/tunisia/tunisia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/mauritania/mauritania.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/united_republic_of_tanzania/united_republic_of_tanzania.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/libya/libya.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/nigeria/nigeria.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/burkina_faso/burkina_faso.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/swaziland/swaziland.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/djibouti/djibouti.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/senegal/senegal.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/st_helena/st_helena.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/niger/niger.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/congo/congo.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/central_african_republic/central_african_republic.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/guinea-bissau/guinea-bissau.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/mayotte/mayotte.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/gambia/gambia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/rwanda/rwanda.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/zimbabwe/zimbabwe.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/ethiopia/ethiopia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/gabon/gabon.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/south_africa/south_africa.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/democratic_republic_of_the_congo/democratic_republic_of_the_congo.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/mozambique/mozambique.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/africa/liberia/liberia.shapefiles.zip
+
+wget -nH -x http://downloads.cloudmade.com/antarctica//.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/antarctica/french_southern_and_antarctic_lands/french_southern_and_antarctic_lands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/antarctica/heard_island_and_mcdonald_islands/heard_island_and_mcdonald_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/antarctica/bouvet_island/bouvet_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/antarctica/sgssi/sgssi.shapefiles.zip
+
+wget -nH -x http://downloads.cloudmade.com/asia/iraq/iraq.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/laos/laos.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/turkmenistan/turkmenistan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/pakistan/pakistan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/turkey/turkey.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/yemen/yemen.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/gaza_strip/gaza_strip.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/japan/japan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/south_korea/south_korea.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/iran/iran.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/mongolia/mongolia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/paracel_islands/paracel_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/russian_federation/russian_federation.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/lebanon/lebanon.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/vietnam/vietnam.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/spratly_islands/spratly_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/bangladesh/bangladesh.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/united_arab_emirates/united_arab_emirates.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/kyrgyzstan/kyrgyzstan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/taiwan/taiwan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/oman/oman.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/kazakhstan/kazakhstan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/thailand/thailand.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/union_of_myanmar/union_of_myanmar.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/nepal/nepal.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/christmas_island/christmas_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/israel/israel.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/syria/syria.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/india/india.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/malaysia/malaysia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/kuwait/kuwait.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/tajikistan/tajikistan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/bhutan/bhutan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/uzbekistan/uzbekistan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/bahrain/bahrain.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/macau/macau.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/cambodia/cambodia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/brunei/brunei.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/democratic_republic_of_timor-leste/democratic_republic_of_timor-leste.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/maldives/maldives.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/british_indian_ocean_territory/british_indian_ocean_territory.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/north_korea/north_korea.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/china/china.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/saudi_arabia/saudi_arabia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/singapore/singapore.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/sri_lanka/sri_lanka.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/indonesia/indonesia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/afghanistan/afghanistan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/philippines/philippines.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/qatar/qatar.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/asia/jordan/jordan.shapefiles.zip
+
+wget -nH -x http://downloads.cloudmade.com/europe/europe.shapefiles.zip
+
+wget -nH -x http://downloads.cloudmade.com/north_america/dominica/dominica.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/anguilla/anguilla.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/virgin_islands/virgin_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/netherlands_antilles/netherlands_antilles.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/st_vincent_and_the_grenadines/st_vincent_and_the_grenadines.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/nicaragua/nicaragua.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/grenada/grenada.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/trinidad_and_tobago/trinidad_and_tobago.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/guadeloupe/guadeloupe.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/guatemala/guatemala.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/st_pierre_and_miquelon/st_pierre_and_miquelon.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/cuba/cuba.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/st_lucia/st_lucia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/belize/belize.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/puerto_rico/puerto_rico.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/barbados/barbados.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/jamaica/jamaica.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/bahamas/bahamas.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/martinique/martinique.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/costa_rica/costa_rica.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/aruba/aruba.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/mexico/mexico.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/honduras/honduras.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/haiti/haiti.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/antigua_and_barbuda/antigua_and_barbuda.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/dominican_republic/dominican_republic.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/canada.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/pr_edwrd_island/canada/pr_edwrd_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/saskatchewan/canada/saskatchewan.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/yukon/canada/yukon.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/new_brunswick/canada/new_brunswick.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/nunavut/canada/nunavut.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/manitoba/canada/manitoba.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/nova_scotia/canada/nova_scotia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/newfoundland/canada/newfoundland.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/british_columbia/canada/british_columbia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/quebec/canada/quebec.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/alberta/canada/alberta.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/ontario/canada/ontario.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/canada/nw_territories/canada/nw_territories.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/panama/panama.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/bermuda/bermuda.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/british_virgin_islands/british_virgin_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/greenland/greenland.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/federation_of_saint_kitts_and_nevis/federation_of_saint_kitts_and_nevis.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/north_america/el_salvador/el_salvador.shapefiles.zip
+
+wget -nH -x http://downloads.cloudmade.com/oceania/tokelau/tokelau.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/new_caledonia/new_caledonia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/republic_of_palau/republic_of_palau.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/marshall_islands/marshall_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/howland_island/howland_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/tonga/tonga.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/american_samoa/american_samoa.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/new_zealand/new_zealand.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/northern_mariana_islands/northern_mariana_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/papua_new_guinea/papua_new_guinea.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/fiji/fiji.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/baker_island/baker_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/jarvis_island/jarvis_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/norfolk_island/norfolk_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/niue/niue.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/cook_islands/cook_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/australia/australia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/johnston_atoll/johnston_atoll.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/guam/guam.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/wallis_and_futuna/wallis_and_futuna.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/vanuatu/vanuatu.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/independent_state_of_samoa/independent_state_of_samoa.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/french_polynesia/french_polynesia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/cocos_keeling_islands/cocos_keeling_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/kiribati/kiribati.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/federated_states_of_micronesia/federated_states_of_micronesia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/tuvalu/tuvalu.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/wake_island/wake_island.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/pitcairn_islands/pitcairn_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/midway_islands/midway_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/solomon_islands/solomon_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/oceania/nauru/nauru.shapefiles.zip
+
+wget -nH -x http://downloads.cloudmade.com/south_america/bolivia/bolivia.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/chile/chile.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/falkland_islands/falkland_islands.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/ecuador/ecuador.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/peru/peru.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/brazil/brazil.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/paraguay/paraguay.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/argentina/argentina.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/suriname/suriname.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/guyana/guyana.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/uruguay/uruguay.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/french_guiana/french_guiana.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/venezuela/venezuela.shapefiles.zip
+wget -nH -x http://downloads.cloudmade.com/south_america/colombia/colombia.shapefiles.zip
+
+OSM_base=`pwd`
+for file in `find . -name "*.zip"`
+do
+
+	base=`basename $file`
+	dir=`dirname $file`
+	echo "Unzipping $base in $dir"
+
+	cd $dir
+	unzip $base
+	cd $OSM_base
+
+done
